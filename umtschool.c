2abd0848d5c29d5c672198767337dd0215f3467c
erlin-08 <Erlin.cala25@umt.edu.al>
2025-11-14 09:52:28 +0100

    Add files via upload

--- umtschool.c
+++ umtschool.c
@@ -3,8 +3,8 @@
 int main(){
 int array[]={1,2,3,4,5};
 
-for(int i=0; i<4;i+=){
-printf("Elementet ne varg jane: %d" , array[i]);
+for(int i=0; i<4;i++){
+printf("Elementet ne varg jane: %d\n" , array[i]);
 }
 |
 return 0;
